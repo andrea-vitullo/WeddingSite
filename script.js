6337fabc3bec71314bfab0414a93c7a1845a163f
Andrea Vitullo <andrea.vitullo27@gmail.com>
2025-04-07 10:26:25 +0100

    remove prevent validation

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ document.getElementById('add-guest').addEventListener('click', function() {
   
   // Optional: Additional form submission handling or validation can go here.
   document.getElementById('rsvp-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent default submission for demonstration
     const messageDiv = document.getElementById('message');
     messageDiv.textContent = 'Grazie eh!';
   });
